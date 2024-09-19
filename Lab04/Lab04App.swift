8bc05d0350a08bd5212dc719b1946d5167f92187
mitrajesse <mitrajesse@gmail.com>
2024-09-19 17:40:59 -0400

    Initial Commit

---
+++ Lab04/Lab04App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab04App.swift
+//  Lab04
+//
+//  Created by Jesse Mitra on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab04Tests/Lab04Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab04Tests.swift
+//  Lab04Tests
+//
+//  Created by Jesse Mitra on 9/19/24.
+//
+
+import Testing
+@testable import Lab04
+
+struct Lab04Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
